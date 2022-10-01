21e91eadf0eaa0eb6c1839a250574add2c1e974a
PrincessMalu <malavika363.nair@gmail.com>
2022-10-01 16:05:28 -0400

    commit with content.js file

--- content.js
+++ content.js
@@ -3,5 +3,5 @@ console.log( "This is my extension");
 let paragraphs = document.getElementsByTagName('p')
 for (elt of paragraphs)
 {
-    elt.style['background-color'] = '#FF00FF';
+    elt.style['background-color'] = '#FF10FF';
 }
