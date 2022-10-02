e21af5da42bf591e8b5062c6579276cca608a667
shanayabarretto <barretto.shanaya@gmail.com>
2022-10-02 01:27:54 -0400

    removed unnecessary files, heroku app is up

--- Procfile
+++
@@ -1,1 +0,0 @@
-web: python Scrape_Google.py
--- content.js
+++ content.js
@@ -1,3 +1,2 @@
-console.log( "This is my extension");
 let brand = document.querySelector('table tbody .po-brand td:nth-child(2) span');
 console.log(brand.textContent)
